5b7c48a9436b785c922647d42e0f1c0a8612badf
Marilena Sales <marilena.sales@gmail.com>
2022-05-29 14:23:35 -0300

    Arus

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using dotnet_robot_achilles.src.Entities;
+
+namespace dotnet_robot_achilles{
+  class Program{
+    static void Main (string[] args)
+    {
+      Arus hero = new Arus("Arus", 23, "knight");
+      //caixinha azul propriedade roxa: método
+      
+
+      Console.Write("");
+    }
+  }
+}
--- src/Entities/Arus.cs
+++ src/Entities/Arus.cs
@@ -2,6 +2,15 @@ namespace dotnet_robot_achilles.src.Entities
 {
     public class Arus
     {
-        public string nome;
+        //construtor
+        public Arus(string Name, int Level, string HeroType){
+            //this = esse //ta olhando para ele mesmo, para não se confundir com outro name de outra classe
+            this.Name = Name;
+            this.Level= Level;
+            this.HeroType = HeroType;
+        }
+        public string Name;
+        public int Level;
+        public string HeroType;
     }
 }
