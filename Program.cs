cd38f0003170e0bc1825cab26fa7752df299228a
Marilena Sales <marilena.sales@gmail.com>
2022-05-29 15:33:54 -0300

    Criando múltiplas instâncias a partir de uma mesma classe

--- Program.cs
+++ Program.cs
@@ -6,10 +6,12 @@ namespace dotnet_robot_achilles{
     static void Main (string[] args)
     {
       Arus hero = new Arus("Arus", 23, "knight");
+      //Arus oponnet = new Arus("Maleficus", 99, "Devil");
+      Arus wizard = new Arus("Jennica", 23, "White Wizard");
       //caixinha azul propriedade roxa: método
       
 
-      Console.Write("");
+      Console.Write(wizard.Attack());
     }
   }
 }
--- src/Entities/Arus.cs
+++ src/Entities/Arus.cs
@@ -12,5 +12,20 @@ namespace dotnet_robot_achilles.src.Entities
         public string Name;
         public int Level;
         public string HeroType;
+
+        //método 
+        /*
+        String não retorna nada
+        Override = sobreescrita de método
+        */
+        public override string ToString(){
+            return this.Name + " " + this.Level + " " + this.HeroType;
+        }
+
+        //método para atacar
+        public string Attack(){
+            return this.Name + " Atacou com a sua espada";
+        }
+
     }
 }
